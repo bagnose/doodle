2d67c94d6555040e73ddd0ad929930885b9c1aa1
bagnose <bagnose@gmail.com>
2012-10-13 22:38:03 +1030

    Significant changes to dupes. Doodle out of build until GtkD builds with dmd-2.061.

--- doodle/main/dupes.d
+++ doodle/main/dupes.d
@@ -1,23 +1,27 @@
 import std.stdio;
+import std.path;
 import std.string;
 import std.exception;
 import std.algorithm;
 import std.file;
-import std.md5;
+import std.digest.md;
 import std.getopt;
 import std.conv;
 import std.ascii;
 import std.c.stdlib;
 
-ulong string_to_size(string s) {
-    // Convert strings to sizes, eg:
-    //   "50"   -> 50
-    //   "80B"  -> 80
-    //   "10K"  -> 10240
-    //   "1M"   -> 1048576
-    // Throws ConvException
-
-    immutable map = [ 'B':1UL, 'K':1UL<<10, 'M':1UL<<20, 'G':1UL<<30, 'T':1UL<<40 ];
+// Convert strings to sizes, eg:
+//   "50"   -> 50
+//   "80B"  -> 80
+//   "10K"  -> 10240
+//   "1M"   -> 1048576
+// Throws ConvException
+ulong stringToSize(string s) {
+    immutable map = [
+        'B':1UL,
+        'K':1UL<<10, 'M':1UL<<20, 'G':1UL<<30,
+        'T':1UL<<40, 'P':1UL<<50, 'E':1UL<<60
+        ];
 
     if (s.length == 0) {
         throw new ConvException("Empty string");
@@ -35,52 +39,72 @@ ulong string_to_size(string s) {
                 throw new ConvException(format("Bad size unit character: %s", s[$-1]));
             }
 
-            s = s[0..$-1];
+            s = s[0 .. $-1];
         }
 
         return multiplier * to!ulong(s);
     }
 }
 
-string size_to_string(in ulong size) {
-    /+
-    immutable array = [ 'B', 'K', 'M', 'G', 'T' ];
-    size_t index = 0;
+// Convert size to strings, eg:
+//   10240 -> "10K"
+string sizeToString(in ulong size) {
+    static int highestBitIndex(ulong value) {
+        for (int i = value.sizeof * 8 - 1; i != 0; --i) {
+            if ((value >> i) & 1) return i;
+        }
 
-    foreach (i, c; array) {
-        if (size / (1UL << i
-
-        writefln("%s %s", i, c);
+        return 0;
     }
-    +/
 
-    return format("%sK", size / 1024);
+    // We don't want 1100 to become 1K, but 11000 can be 11K
+    int bitIndex = highestBitIndex(size / 10);
+
+    int unitIndex = bitIndex / 10;
+    immutable array = [ 'B', 'K', 'M', 'G', 'T', 'P', 'E' ];
+
+    return format("%s%s", (size >> (unitIndex * 10)), array[unitIndex]);
 }
 
-void find_duplicates(in string[] dirs,
-                     in ulong    file_size,
-                     in ulong    digest_size,
-                     bool        verbose) {
-    static ubyte[16] compute_md5(in string filename, in ulong max_bytes) {
-        size_t chunk_size = min(max_bytes, 4096 * 1024);
-        ubyte[16] digest;
+string upDir(in string path, uint dirCount) {
+    string result = path;
+    for (uint i = 0; i != dirCount; ++i) {
+        result = dirName(result);
+    }
+    return result;
+}
 
+void findDuplicates(in string[] dirs,
+                    in ulong    minFileSize,
+                    in ulong    compareAmount,
+                    bool        verbose) {
+    alias ubyte[16] Hash;
+
+    static Hash computeHash(in string filename, in ulong maxBytes) {
+        //writefln("Computing hash of %s", filename);
         auto file = File(filename, "r");
         scope(exit) file.close();
 
-        MD5_CTX context;
-        context.start();
-        ulong byte_count = 0;
-        foreach (ubyte[] buffer; chunks(file, chunk_size)) {
-            context.update(buffer);
-            byte_count += buffer.length;
-            if (byte_count >= max_bytes) {
+        MD5 md5;
+        ulong byteCount = 0;
+
+        foreach (ubyte[] buffer; chunks(file, 1024 * 4096)) {
+            write("#");
+            stdout.flush();
+            if (maxBytes != 0 && byteCount + buffer.length >= maxBytes) {
+                md5.put(buffer[0 .. maxBytes - byteCount]);
                 break;
             }
+            else {
+                md5.put(buffer);
+                byteCount += buffer.length;
+            }
         }
-        context.finish(digest);
 
-        return digest;
+        Hash hash = md5.finish();
+        //writefln(" %s", toHexString(hash));
+        writefln(" %s", hash);
+        return hash;
     }
 
     struct FileInfo {
@@ -88,159 +112,208 @@ void find_duplicates(in string[] dirs,
         ulong  size;
     }
 
-    FileInfo[] file_array;
+    static void accumulateFileInfo(in string dir, in ulong minFileSize, ref FileInfo[] fileArray) {
+        try {
+            //writefln("Dir: %s", dir);
+            foreach (string entry; dirEntries(dir, SpanMode.shallow, false)) {
+                if (isSymlink(entry)) {
+                    continue;
+                }
+
+                if (isDir(entry)) {
+                    // Recurse
+                    accumulateFileInfo(entry, minFileSize, fileArray);
+                }
+                else if (isFile(entry)) {
+                    ulong size = getSize(entry);
+                    //writefln("File: %s, %s", entry, sizeToString(size));
+                    if (size >= minFileSize) {
+                        fileArray ~= FileInfo(entry, size);
+                        writef("\r%s", fileArray.length);
+                        stdout.flush();
+                    }
+                }
+            }
+        }
+        catch (FileException ex) {
+            writeln(ex.msg);
+        }
+    }
+
+    FileInfo[] fileArray;
 
     writefln("Accumulating file list");
 
     foreach (string dir; dirs) {
-        if (isDir(dir)) {
-            string last_entry;
-            try {
-                foreach (string filename; dirEntries(dir, SpanMode.depth, false)) {
-                    last_entry = filename;
-                    try {
-                        if (!isSymlink(filename) && isFile(filename)) {
-                            ulong size = getSize(filename);
-                            if (size >= file_size) {
-                                file_array ~= FileInfo(filename, size);
-                            }
-                        }
-                    }
-                    catch (Exception ex) {
-                        writefln("Skipping %s", filename);
-                        //writefln("Exception %s", ex);
-                        // TODO accumulate errors and print after traversal
-                    }
-                }
-            }
-            catch (FileException ex) {
-                // ignore
-                writefln("Error, dirEntries bailed out after: %s. Continuing anyway", last_entry);
-            }
-        }
-        else {
-            writefln("Not a dir: %s", dir);
-        }
+        accumulateFileInfo(dir, minFileSize, fileArray);
     }
+    writeln();
 
-    writefln("Processing %s files", file_array.length);
+    writefln("Processing %s files", fileArray.length);
 
-    uint[][ulong] size_to_file_indices;
-    bool[ulong]   duplicate_sizes;
+    uint[][ulong] sizeToFileIndices;
+    bool[ulong]   duplicateSizes;
 
-    foreach (uint index, file; file_array) {
+    foreach (uint index, file; fileArray) {
         //writefln("%s %s %s", index, file.name, file.size);
 
-        if (uint[] * indices = (file.size in size_to_file_indices)) {
-            if (indices.length == 1) {
+        if (uint[] * indicesSameSize = (file.size in sizeToFileIndices)) {
+            if (indicesSameSize.length == 1) {
                 // Second time we've seen a file of this size,
-                // record it in the duplicate_sizes array
-                duplicate_sizes[file.size] = true;
+                // record it in the duplicateSizes array
+                duplicateSizes[file.size] = true;
             }
 
-            (*indices) ~= index;
+            (*indicesSameSize) ~= index;
         }
         else {
-            size_to_file_indices[file.size] = [ index ];
+            sizeToFileIndices[file.size] = [ index ];
         }
     }
 
-    writefln("Number of files of duplicate size %s", duplicate_sizes.length);
+    writefln("Number of files of duplicate size %s", duplicateSizes.length);
 
-    ulong total_waste = 0;
+    ulong totalWaste = 0;
 
-    foreach_reverse (size; duplicate_sizes.keys.sort) {
-        uint[] indices = size_to_file_indices[size];
-        //writefln("For size %s there are %s files", size, indices.length);
+    foreach_reverse (size; duplicateSizes.keys.sort) {
+        uint[] indicesSameSize = sizeToFileIndices[size];
+        //writefln("For size %s there are %s files", size, indicesSameSize.length);
 
-        uint[][ubyte[16]] digest_to_indices;
+        uint[][Hash] hashToIndices;
 
-        foreach (index; indices) {
-            const FileInfo file_info = file_array[index];
+        foreach (index; indicesSameSize) {
+            const FileInfo fileInfo = fileArray[index];
 
             try {
-                ubyte[16] digest = compute_md5(file_info.name, digest_size);
+                writefln("(%s) %s", sizeToString(fileInfo.size), fileInfo.name);
+                Hash hash = computeHash(fileInfo.name, compareAmount);
 
-                if (uint[] * duplicate_indices = (digest in digest_to_indices)) {
-                    // A true duplicate
-                    // index and index2 are the same
-
-                    (*duplicate_indices) ~= index;
+                if (uint[] * duplicateIndices = (hash in hashToIndices)) {
+                    (*duplicateIndices) ~= index;       // a duplicate
                 }
                 else {
-                    digest_to_indices[digest] ~= index;
+                    hashToIndices[hash] ~= index;       // the first instance
                 }
             }
             catch (ErrnoException ex) {
-                //writefln("Skipping: %s", file_info.name);
+                writeln(ex.msg);
             }
-
-            //writefln("\t%s", file_info.name);
         }
 
-        foreach (indices2; digest_to_indices) {
-            if (indices2.length > 1) {
+        foreach (indicesSameHash; hashToIndices) {
+            if (indicesSameHash.length > 1) {
                 // List the duplicates
-                foreach (i, index; indices) {
-                    FileInfo file_info = file_array[index];
+                foreach (i, index; indicesSameHash) {
+                    FileInfo fileInfo = fileArray[index];
                     if (i == 0) {
-                        writefln("%s", size_to_string(file_info.size));
-                        total_waste += file_info.size;
+                        writefln("%s", sizeToString(fileInfo.size));
+                        totalWaste += fileInfo.size;
                     }
-                    writefln("    %s", file_info.name);
+                    writefln("[%s]\t%s", i, fileInfo.name);
                 }
-                writefln("");
+                writef("Select index to keep (deleting others) or enter to ignore (prepend hyphens to ascend dirs): ");
+                string s = readln().chomp();
+
+                try {
+                    uint dirCount = 0;
+
+                    while (s.length != 0) {
+                        if (s[0] == '-') {
+                            ++dirCount;
+                            s = s[1 .. $];
+                        }
+                        else {
+                            break;
+                        }
+                    }
+
+                    uint i_keep = to!uint(s);
+
+                    if (i_keep >= 0 && i_keep < indicesSameHash.length) {
+                        foreach (i, index; indicesSameHash) {
+                            FileInfo fileInfo = fileArray[index];
+                            if (i != i_keep) {
+                                try {
+                                    if (dirCount > 0) {
+                                        string dir = upDir(fileInfo.name, dirCount);
+                                        writefln(" ** Removing directory: %s", dir);
+                                        //rmdirRecurse(dir);
+                                    }
+                                    else {
+                                        writefln(" ** Removing file: %s", fileInfo.name);
+                                        //remove(fileInfo.name);
+                                    }
+                                }
+                                catch (Exception ex) {
+                                    writefln(ex.msg);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (ConvException ex) {
+                    // Silently ignore
+                    //writefln("No good: %s", ex.msg);
+                }
             }
         }
     }
 
-    writefln("Done, total waste: %s", size_to_string(total_waste));
+    writefln("Done, total waste: %s", sizeToString(totalWaste));
 }
 
 int main(string[] args) {
-    ulong file_size;
-    ulong digest_size;
+    ulong minFileSize;
+    ulong compareAmount;
     bool  verbose;
 
     try {
-        string file_size_string   = "100K";
-        string digest_size_string = "100K";
+        string minFileSizeString   = "1M";
+        string compareAmountString = "0";
 
-        void help(in string) {
+        void help(in string = "") {
             writefln("Usage: dupes [OPTION]... DIR...\n"
                      "Recursively locate duplicate files in a list of directories\n"
                      "\n"
                      "Options\n"
-                     " -d, --digest-size=SIZE     size of digest used for comparison [%s]\n"
-                     " -f, --file-size=SIZE       minimum size of files searched for duplication [%s]\n"
-                     " -v, --verbose              be verbose\n"
+                     " -m, --min-file-size=SIZE   minimum size of files to be considered [%s]\n"
+                     " -c, --compare-amount=SIZE  number of bytes to compare [%s] (0 -> unlimited)\n"
                      "     --help                 display this help and exit\n"
                      "\n"
-                     "SIZE is an integer, optionally followed by K, M, G, T",
-                     file_size_string,
-                     digest_size_string);
+                     "SIZE is an integer, optionally followed by B, K, M, G, T, P, E",
+                     minFileSizeString,
+                     compareAmountString);
             exit(1);
         }
 
         getopt(args,
-               "file-size|f",   &file_size_string,
-               "digest-size|d", &digest_size_string,
-               "verbose|v",     &verbose,
-               "help",          &help);
+               "min-file-size|m",  &minFileSizeString,
+               "compare-amount|c", &compareAmountString,
+               "verbose|v",        &verbose,
+               "help",             &help);
 
-        file_size   = string_to_size(file_size_string);
-        digest_size = string_to_size(digest_size_string);
+        minFileSize   = stringToSize(minFileSizeString);
+        compareAmount = stringToSize(compareAmountString);
+
+        if (verbose) {
+            writefln("min-file-size=%s, compare-amount=%s",
+                     sizeToString(minFileSize),
+                     compareAmount ? sizeToString(compareAmount) : "(unlimited)");
+        }
+
+        string[] dirs = args[1 .. $];
+
+        if (dirs.length == 0) {
+            help();
+        }
+        else {
+            findDuplicates(dirs, minFileSize, compareAmount, verbose);
+        }
     }
-    catch (ConvException ex) {
-        writefln("Conversion error: %s", ex);
+    catch (Exception ex) {
+        writeln(ex.msg);
         exit(2);
     }
-
-    if (verbose) {
-        writefln("file-size=%s, digest-size=%s", size_to_string(file_size), size_to_string(digest_size));
-    }
-
-    find_duplicates(args[1..$], file_size, digest_size, verbose);
 
     return 0;
 }
