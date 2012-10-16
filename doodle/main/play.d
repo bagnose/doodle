372c4cc262d4b3ae1944f0d1461bc80cc315511e
bagnose <bagnose@gmail.com>
2012-10-16 17:39:32 +1030

    Small tinkering with hash accumulators

--- doodle/main/play.d
+++ doodle/main/play.d
@@ -2,66 +2,70 @@ import std.stdio;
 import std.file;
 import std.digest.md;
 
-struct ByteAccumulator {
-    alias ubyte[1] Hash;
+struct ByteAccumulator(size_t hashSize) {
+    alias ubyte[hashSize] Hash;
 
     this(in string filename) {
         _file = File(filename);
     }
 
-    bool accumulate(out Hash hash) {
-        ubyte[1] buffer;
-        ubyte[] slice = _file.rawRead(buffer);
+    void accumulate() {
+        ubyte[] slice = _file.rawRead(_hash);
 
-        if (slice.length == 1) {
-            hash = slice;
-            return true;
+        if (slice.length == _hash.length) {
+            _ongoing = true;
         }
         else {
-            hash[0] = 0;
-            return false;
+            _hash[slice.length .. $] = 0;
+            _ongoing = false;
         }
     }
 
+    @property Hash hash()    const { return _hash; }
+    @property bool ongoing() const { return _ongoing; }
+
     private {
         File    _file;
+        Hash    _hash;
+        bool    _ongoing;
     }
 }
 
 struct MD5Accumulator {
     alias ubyte[16] Hash;
 
-    this(in string filename) {
+    this(in string filename, size_t chunkSize = 64 * 1024) {
         _file = File(filename);
-        _buffer = new ubyte[64 * 1024];
+        _buffer = new ubyte[chunkSize];
     }
 
-    bool accumulate(out Hash hash) {
+    void accumulate() {
         ubyte[] slice = _file.rawRead(_buffer);
         _md5.put(slice);
-        hash = _md5.finish();
-        return slice.length == _buffer.length;
+        _hash    = _md5.finish();
+        _ongoing = slice.length == _buffer.length;
     }
+
+    @property Hash hash()    const { return _hash; }
+    @property bool ongoing() const { return _ongoing; }
 
     private {
         File    _file;
         MD5     _md5;
         ubyte[] _buffer;
+        Hash    _hash;
+        bool    _ongoing;
     }
 }
 
 int main(string[] args) {
     foreach (path; args[1 .. $]) {
-        //auto ha = MD5Accumulator(path);
-        auto ha = ByteAccumulator(path);
-        for (;;) {
-            ha.Hash hash;
-            bool ongoing = ha.accumulate(hash);
-            writefln(" %s", toHexString(hash));
-            if (!ongoing) {
-                break;
-            }
-        }
+        //auto ha = MD5Accumulator(path, 16);
+        auto ha = ByteAccumulator!(16)(path);
+        do {
+            ha.accumulate();
+            writefln(" %s", toHexString(ha.hash));
+        } while (ha.ongoing);
     }
     return 0;
 }
